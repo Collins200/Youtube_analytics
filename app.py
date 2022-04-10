4269270ecf3e4ccb62bd3e9232c3dd7c29bed462
Collins lel <collinscollolel@gmail.com>
2022-04-10 19:28:22 +0300

    good progress

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from unicodedata import name
 from flask import Flask,render_template,url_for,request
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 from pytrends.request import TrendReq
 import pycountry
 import numpy as np
@@ -12,6 +14,25 @@ import os
 pytrends = TrendReq(hl='en-US',timeout=None,retries=4)
 
 app = Flask(__name__)
+# 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://frlfqedqxcgdgc:7c686ae3294a9352b3dbc71a2153cf304449622e2524080eee3972f1cbaaf4fe@ec2-52-73-155-171.compute-1.amazonaws.com:5432/d4pfojjqsai9bd'
+db = SQLAlchemy()
+db.init_app(app)
+migrate = Migrate(app,db)
+
+# model 
+
+class Youtubers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Country=db.Column(db.String(100), unique=True, nullable=False)
+    YoutuberName=db.Column(db.String(100),  nullable=False)
+    ContentType=db.Column(db.String(100),  nullable=False)
+
+
+
+    def __repr__(self):
+        return '<Youtuber %r>' % self.YoutuberName
 
 @app.route("/",methods=['GET', 'POST'])
 def index():
@@ -47,7 +68,7 @@ def index():
         searchword=searchword.capitalize()
         searchword=searchword.replace(" ","%20")
 
-        country= request.form.get("inputState")
+        my_country= request.form.get("inputState")
         x=[item for item in results if item["text"] == country][0]
         codes = x["key"]
         codes=codes.upper()
@@ -58,7 +79,7 @@ def index():
     
     
     # states=state.pop()
-    keyword=['Dog']
+    keyword=['Computer']
     # extract data about keywords
     # your_input=str(input('Enter the keyword: '))
     # your_input='Computer'
@@ -69,19 +90,21 @@ def index():
 
     # pytrends.build_payload(keyword,timeframe=f'2020-02-26 {date.today()}', gprop = 'youtube', geo='AU')
     
-    pytrends.build_payload(keyword,timeframe='today 5-y', gprop = 'youtube', geo='KE')
+    pytrends.build_payload(keyword,timeframe='today 5-y', gprop = 'youtube', geo='AU')
     # word=keyword.pop()
-    word='Dog'
+    word='Computer'
     
     # specify and get data
     data= pytrends.interest_over_time()
     data = data.drop(labels=['isPartial'],axis='columns')
     data.index.name = None
     data.columns=[word]
+    # dataa=data.to_dict()
+
     plt.plot(data,label= word)
    
     # add titles
-    plt.suptitle("Graphical analysis of keyword searches on youtube")
+    plt.suptitle("Graphical analysis of keyword searches on youtube as a percentage")
     plt.xlabel('period')
     plt.ylabel('searches')
 
@@ -96,6 +119,7 @@ def index():
     data1=pytrend.interest_by_region(resolution='COUNTRY',inc_low_vol=True)
     data1.index.name = None
     data1.columns=[word]
+    # dataa1=data1.to_dict()
 
  
     
@@ -105,7 +129,7 @@ def index():
 
 
     # add titles
-    plt.suptitle("Contries with highest searches of the keyword '{}'".format(word))
+    plt.suptitle("Contries with highest searches of the keyword '{}' as a percentage".format(word))
     plt.xlabel("Countries")
     plt.ylabel("average number of searches")
     plt.legend(loc='best', shadow=True)
@@ -124,12 +148,12 @@ def index():
 
     target = os.path.join(app.static_folder, 'youtubers.csv')
     df = pd.read_csv(target)
-    df.columns=['Country','YoutuberName','ContentType']
+    df.columns=['id','Country','YoutuberName','ContentType']
 
 
     
-    ContentType=df[df.iloc[:,0]=='Kenya'].iloc[0,2]
-    YoutuberName=df[df.iloc[:,0]=='Kenya'].iloc[0,1]
+    ContentType=df[df.iloc[:,1]=='Kenya'].iloc[0,3]
+    YoutuberName=df[df.iloc[:,1]=='Kenya'].iloc[0,2]
 
     # test
     df_queries = pytrends.related_queries()
@@ -141,10 +165,13 @@ def index():
 
     ###NOTE!!!!Let it print "Enter a more valid word in case of error"
 
-    return render_template('index.html',al=al,word=word,one=one,two=two,three=three,Country=[],table1=[data1.to_html(classes='data1', header="true")],
-    four=four,period=period,YoutuberName=YoutuberName,ContentType=ContentType,tables=[data.to_html(classes='data', header="true")],top_related_queries=[top_related_queries.to_html(classes='data', header="true")],rising_related_queries=[rising_related_queries.to_html(classes='data', header="true")],
+    return render_template('index.html',al=al,word=word,one=one,two=two,three=three,Country=[],
+    table1=[data1.to_html(classes='data1 table', header="true")],four=four,period=period,
+    YoutuberName=YoutuberName,ContentType=ContentType,tables=[data.to_html(classes='data  table', header="true")],
+    top_related_query=[top_related_queries.to_html(classes='data table', header="true")if( top_related_queries is not None) else "No related keyword in your country"],
+    rising_related_query=[rising_related_queries.to_html(classes='data table', header="true") if(rising_related_queries is not None) else "No related keyword in your country"],
     url='/static/images/plot.png',
-    url1='/static/images/plot1.png',table3=[df.to_html(classes='data3')])
+    url1='/static/images/plot1.png',table3=[df.to_html(classes='data3 table')])
 
 if __name__ =='__main__':
 
