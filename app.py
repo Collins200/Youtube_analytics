01b157a5405b56e9955f8d68a9243ea90788e639
Collins lel <collinscollolel@gmail.com>
2022-04-10 03:22:53 +0300

    few addition

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ import matplotlib.pyplot as plt
 import os
 # create a pytrends object.request data from Google trends
 # pytrends = TrendReq(hl='en-US',requests_args={'verify':False})
-pytrends = TrendReq(hl='en-US')
+pytrends = TrendReq(hl='en-US',timeout=None,retries=4)
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=['GET', 'POST'])
 def index():
 
     results = []
@@ -22,19 +22,25 @@ def index():
             "key": country.alpha_2,
             "text": "{}".format(country.name, country.alpha_2),
             })
-    al=[]
-    for d in results:
-        a=(d['text'])
-        al.append(a)
+
+    # results=[{"key": country.alpha_2,"text": "{}".format(country.name, country.alpha_2),}for country in pycountry.countries]
+
+    # al=[]
+    # for d in results:
+    #     a=(d['text'])
+    #     al.append(a)
+    al=[d['text'] for d in results]
+
     
     timeframes = [{'key': 'today 5-y', 'text': '5 year period'},
                   {'key': 'today 12-m', 'text': '1 year period'},
                   {'key': 'today 3-m', 'text': '3 months period'},
                   {'key': 'today 1-m', 'text': '1 month period'}]
-    period=[]
-    for a in timeframes:
-        d=(a['text'])
-        period.append(d)
+    # period=[]
+    # for a in timeframes:
+    #     d=(a['text'])
+    #     period.append(d)
+    period=[(a['text']) for a in timeframes]
         
     if request.method=='POST':        
         searchword = request.form.get("searchword")
@@ -52,7 +58,7 @@ def index():
     
     
     # states=state.pop()
-    keyword=['Fitness']
+    keyword=['Dog']
     # extract data about keywords
     # your_input=str(input('Enter the keyword: '))
     # your_input='Computer'
@@ -61,9 +67,11 @@ def index():
     # keyword.insert(0, your_input)
     # pytrends.build_payload(keyword,timeframe= 'today 3-m',gprop = 'youtube')
 
-    pytrends.build_payload(keyword,timeframe=f'2020-02-26 {date.today()}', gprop = 'youtube', geo='AU')
+    # pytrends.build_payload(keyword,timeframe=f'2020-02-26 {date.today()}', gprop = 'youtube', geo='AU')
+    
+    pytrends.build_payload(keyword,timeframe='today 5-y', gprop = 'youtube', geo='KE')
     # word=keyword.pop()
-    word='Fitness'
+    word='Dog'
     
     # specify and get data
     data= pytrends.interest_over_time()
@@ -81,18 +89,23 @@ def index():
     plt.legend(loc='best', shadow=True)
     plt.savefig('static/images/plot.png')
 
+    pytrend = TrendReq(hl='en-US',timeout=None,retries=4)
+    pytrend.build_payload(keyword,timeframe='today 5-y', gprop = 'youtube', geo='')
     # extract country-level data about the keywords
-    data1=pytrends.interest_by_region(resolution='COUNTRY',inc_low_vol=True)
+
+    data1=pytrend.interest_by_region(resolution='COUNTRY',inc_low_vol=True)
     data1.index.name = None
     data1.columns=[word]
-    
 
+ 
+    
     dat2=data1.astype(float).nlargest(10, word)
     #plot bar char with Pandas
-    dat2.plot(kind='bar')
+    dat2.plot(kind='bar',color='#ff028a')
+
 
     # add titles
-    plt.suptitle("Contries with highest searches of the keyword {}".format(word))
+    plt.suptitle("Contries with highest searches of the keyword '{}'".format(word))
     plt.xlabel("Countries")
     plt.ylabel("average number of searches")
     plt.legend(loc='best', shadow=True)
@@ -113,13 +126,24 @@ def index():
     df = pd.read_csv(target)
     df.columns=['Country','YoutuberName','ContentType']
 
-    
-    ContentType=df[df.iloc[:,0]=='Zimbabwe'].iloc[0,2]
-    YoutuberName=df[df.iloc[:,0]=='Zimbabwe'].iloc[0,1]
 
-    return render_template('index.html',al=al,word=word,one=one,two=two,three=three,Country=[],
-    four=four,period=period,YoutuberName=YoutuberName,ContentType=ContentType,tables=[data.to_html(classes='data', header="true")],
-    table1=[data1.to_html(classes='data1', header="true")],url='/static/images/plot.png',
+    
+    ContentType=df[df.iloc[:,0]=='Kenya'].iloc[0,2]
+    YoutuberName=df[df.iloc[:,0]=='Kenya'].iloc[0,1]
+
+    # test
+    df_queries = pytrends.related_queries()
+    top_related_queries = df_queries.get(word).get("top")
+    # top_related_queries.sort_values(['value'], ascending = False).head(10).reset_index(drop = True)
+    rising_related_queries= df_queries.get(word).get("rising")
+    # rising_related_queries.sort_values(['value'], ascending = False).head(10)
+
+
+    ###NOTE!!!!Let it print "Enter a more valid word in case of error"
+
+    return render_template('index.html',al=al,word=word,one=one,two=two,three=three,Country=[],table1=[data1.to_html(classes='data1', header="true")],
+    four=four,period=period,YoutuberName=YoutuberName,ContentType=ContentType,tables=[data.to_html(classes='data', header="true")],top_related_queries=[top_related_queries.to_html(classes='data', header="true")],rising_related_queries=[rising_related_queries.to_html(classes='data', header="true")],
+    url='/static/images/plot.png',
     url1='/static/images/plot1.png',table3=[df.to_html(classes='data3')])
 
 if __name__ =='__main__':
